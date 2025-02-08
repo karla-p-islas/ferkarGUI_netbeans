fb9083d7f5b8d1bd1b83cee1259ba3eb40a65243
karla-p-islas <kisaparz@gmail.com>
2025-02-07 18:21:24 -0600

    Update
    IGU 50% on its way, working still on the logic and the logic and the rest of the interfaces

---
+++ app_ferkar/src/main/java/com/ejemplo/app_ferkar/App_ferkar.java
@@ -0,0 +1,17 @@
+package com.ejemplo.app_ferkar;
+
+import com.ejemplo.app_ferkar.IGU.Principal;
+/**
+ *
+ * @author kpaor
+ */
+public class App_ferkar {
+
+    public static void main(String[] args) {
+        
+        Principal princ = new Principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+        
+    }
+}
